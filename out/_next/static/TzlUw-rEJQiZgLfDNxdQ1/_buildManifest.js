fcdd4689f33255f0b3a5897480448808ad405424
Martin Dammrath <martin.dammrath@gmail.com>
2021-03-24 12:31:55 +0100

    add wein-investments

---
+++ out/_next/static/TzlUw-rEJQiZgLfDNxdQ1/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-1dac9a21aa30b3c5bd4f.js"],sortedPages:["\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
