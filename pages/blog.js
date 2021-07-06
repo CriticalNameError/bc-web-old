e94d36aaa501585376400bd30f1838020895f237
Martin Dammrath <martin.dammrath@gmail.com>
2021-07-07 00:47:51 +0200

    try fix display bug

--- pages/blog.js
+++ pages/blog.js
@@ -11,9 +11,16 @@ import ListBlogPosts from "../src/helpers/ListBlogPosts";
 class BlogPage extends React.Component {
   onLeave(origin, destination, direction) {
     console.log("Leaving section " + origin.index);
+    
+    document.getElementById("contact").classList.remove("visible");
+    document.getElementById("vert-nav").classList.remove("pt-5")
   }
   afterLoad(origin, destination, direction) {
     console.log("After load: " + destination.index);
+    if(destination.index === 0){
+      document.getElementById("contact").classList.add("visible")
+      document.getElementById("vert-nav").classList.add("pt-5")
+    }
   }
   render() {
     return (
--- pages/ueber-uns.js
+++ pages/ueber-uns.js
@@ -16,9 +16,16 @@ import { ImageFromCms } from "helpers/utils";
 class AboutUs extends React.Component {
   onLeave(origin, destination, direction) {
     console.log("Leaving section " + origin.index);
+    
+    document.getElementById("contact").classList.remove("visible");
+    document.getElementById("vert-nav").classList.remove("pt-5")
   }
   afterLoad(origin, destination, direction) {
     console.log("After load: " + destination.index);
+    if(destination.index === 0){
+      document.getElementById("contact").classList.add("visible")
+      document.getElementById("vert-nav").classList.add("pt-5")
+    }
   }
   render() {
     return (
